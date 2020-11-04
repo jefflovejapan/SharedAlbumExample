7024db5c02949b69119d63e24c7bea84af1c3966
Jeffrey Blagdon <jeffblagdon@gmail.com>
2020-11-03 21:34:36 -0400

    Initial Commit

---
+++ SharedAlbumExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SharedAlbumExample
+//
+//  Created by Jeffrey Blagdon on 2020-11-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
